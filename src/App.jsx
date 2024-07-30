5310636d1aa12d376be84d313749ae622b9ba817
alinakorobei88 <alinakorobei@gmail.com>
2024-07-30 12:43:53 +0200

    change1

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { lazy, Suspense } from 'react';
+import {Route, Routes} from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+
+function App() {
+  return (
+    <div className='App'>
+      <Navigation />
+      <Suspense fallback={<div>Loading...</div>}>
+       <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/movies' element={<MoviesPage />} />
+        <Route path='/movies/:movieId' element={<MovieDetailsPage />}>
+          <Route path='/cast' element={<MovieCast />} />
+          <Route path='/reviews' element={<MovieReviews />} />
+        </Route>
+        <Route path='*' element={<NotFoundPage />} />
+       </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App
